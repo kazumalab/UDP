9d72f9fc10d4ccd70b93d998c97e6cc3055bf77a
kazumalab <kazuma-arashi@hotmail.co.jp>
2017-11-17 08:52:53 +0900

    Add: MultiCast

--- udp_send.c
+++ udp_send.c
@@ -7,13 +7,16 @@
 int main() {
   int sock;
   struct sockaddr_in addr;
+  char hello[50];
+  printf("メッセージを入力してください");
+  scanf("%s", hello);
 
   sock = socket(AF_INET, SOCK_DGRAM, 0);
   addr.sin_family = AF_INET;
   addr.sin_port = htons(22222);
-  addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr.s_addr);
 
-  sendto(sock, "Hello World!!", 13, 0, (struct sockaddr *)&addr, sizeof(addr));
+  sendto(sock, hello, sizeof(hello), 0, (struct sockaddr *)&addr, sizeof(addr));
   close(sock);
   return 0;
 }
